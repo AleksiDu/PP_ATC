5d3a9b8c88a3bf575dfcb8e1e05c10b1e75b1c62
AleksiDu <aleksi.duluzauri@gmail.com>
2024-02-19 16:57:54 +0400

    add comments and tool change translation

--- index.js
+++ index.js
@@ -2,26 +2,34 @@ import * as fs from "fs";
 import extractPartName from "./Utils/extractPartName.js";
 import extractProjectName from "./Utils/extractProjectName.js";
 
-function parseProbePoints(lines) {
-  let gcode = "";
+function catiaAptToGCode(lines) {
+  let gCode = "";
   let lineNumber = 1;
   for (let line of lines) {
-    if (line.startsWith("GOTO")) {
+    if (line.startsWith("PPRINT") || line.startsWith("TPRINT/")) {
+      gCode += `; ${line.substring(6)}\n`;
+      console.log(line.substring(6));
+    } else if (line.startsWith("LOADTL/")) {
+      const params = line.split("/")[1].split(",");
+      gCode += `N${lineNumber++} T${
+        params[0]
+      }\nN${lineNumber++} M6\nN${lineNumber++} D1\n`;
+    } else if (line.startsWith("GOTO")) {
       let parts = line.split("/");
       let coordinates = parts[1].split(",");
       let [x, y, z] = coordinates.slice(1, 4);
-      gcode += `N${lineNumber++} G0 X${x} Y${y} Z${z}\n`;
+      gCode += `N${lineNumber++} G0 X${x} Y${y} Z${z}\n`;
     } else if (line.startsWith("PROBE/POINTS")) {
       let parts = line.split(",");
-      gcode += `N${lineNumber++} G38.2 Z-${parts[5]} F${parts[3]}\n`;
-      gcode += `N${lineNumber++} G10 L20 P1 Z0.000\n`;
+      gCode += `N${lineNumber++} G38.2 Z-${parts[5]} F${parts[3]}\n`;
+      gCode += `N${lineNumber++} G10 L20 P1 Z0.000\n`;
     } else if (line.startsWith("PROBE/OFF")) {
-      gcode += `N${lineNumber++} G0 Z50.000\n`;
+      gCode += `N${lineNumber++} G0 Z50.000\n`;
     } else if (line.startsWith("RAPID")) {
       // Do nothing, already handled in GOTO
     }
   }
-  return gcode;
+  return gCode;
 }
 
 function main() {
@@ -47,17 +55,17 @@ function main() {
       "; ( PREPARED BY: AD )",
       `; (DATE : ${new Date().toLocaleString()} )`,
       "; ( -------- LIST OF TOOLS ---------- )",
-      "N3 T98_RENISHAW",
+      "; N3 T98_RENISHAW",
       "; ( --------------------------------- )",
       "VERIFY",
       "",
     ].join("\n");
 
-    const gcode = parseProbePoints(lines);
+    const gCode = catiaAptToGCode(lines);
 
-    const finalGcode = headerInfo + gcode;
+    const finalGCode = headerInfo + gCode;
 
-    fs.writeFile(outputFile, finalGcode, (err) => {
+    fs.writeFile(outputFile, finalGCode, (err) => {
       if (err) {
         console.error("Error writing output file:", err);
         return;
